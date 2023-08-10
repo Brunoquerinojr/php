00defe42bba89b2c005a7338d537e6f358f3241c
Brunoquerinojr <brunoquerinosilvajr@gmail.com>
2023-08-10 14:31:57 -0300

    php1

---
+++ aula1/sintaxe1.php
@@ -0,0 +1,8 @@
+<?php
+ // Geração de texto (string)
+ echo "chama sesi-senai";
+ /* Geração de texto estruturado (com tags,atributos) */
+ echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP.<h2>";
+ echo "<h2>Gerando <span class=\"destaque\">HTML</span> através do PHP.<h2>";
+ ?>
+ 
---
+++ aula1/sintaxe3.php
@@ -0,0 +1,14 @@
+<?php
+echo                "testando espaço em branco <br>";
+echo "testando espaço <br>";
+
+//O caracter  &nbsp; adiciona um esoaço em branco dentro da string
+echo "&#160; &#160;                     testando                        espaço               em                  branco<br>";
+echo
+"quebra de linha <br>"
+;
+
+echo "quebra de lih
+nha <br>"
+;
+?>
